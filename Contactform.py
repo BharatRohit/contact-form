972cdc6a84e3eaab9003205eeb491abd56838676
BharatRohit <94052041+BharatRohit@users.noreply.github.com>
2023-01-25 16:11:46 +0530

    Update Contactform.py

--- Contactform.py
+++ Contactform.py
@@ -1,12 +1,9 @@
 import streamlit as st
+<link rel="stylesheet" type="text/css" href="https://github.com/BharatRohit/contact-form/blob/main/style.css">
 st.header(":mailbox: Get In Touch With Me!")
-
-
 Contact_form = """
 <form action="https://formsubmit.co/rohitbharat810@gmail.com" method="POST">
-
 <input type="hidden" name="_captcha" value="false">
-<link rel="stylesheet" type="text/css" href="https://github.com/BharatRohit/contact-form/blob/main/style.css">
      <input type="text" name="name" placeholder="Your name" required>
      <input type="email" name="email" placeholder="Your email" required>
      <textarea name="message" placeholder="Type your message here"></textarea>
@@ -15,7 +12,6 @@ Contact_form = """
 """
 
 st.markdown(Contact_form,unsafe_allow_html=True)
-
 
 # # Use Local Css File
 # def local_css(file_name):
