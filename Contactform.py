ff23c37db396883c5400c7f7b3e7ed12246becee
BharatRohit <94052041+BharatRohit@users.noreply.github.com>
2023-01-25 15:59:02 +0530

    Update Contactform.py

--- Contactform.py
+++ Contactform.py
@@ -13,3 +13,9 @@ Contact_form = """
 """
 
 st.markdown(Contact_form,unsafe_allow_html=True)
+
+# Use Local Css File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}<style>",unsafe_allow_html=True)
+local_css("Style/style.css")
