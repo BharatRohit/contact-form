9c4ef2ef4b3dc060cde3408cfe539732fecda24b
BharatRohit <94052041+BharatRohit@users.noreply.github.com>
2023-01-25 15:45:47 +0530

    Update Contactform.py

--- Contactform.py
+++ Contactform.py
@@ -3,9 +3,13 @@ st.header(":mailbox: Get In Touch With Me!")
 
 Contact_form = """
 <form action="https://formsubmit.co/rohitbharat810@gmail.com" method="POST">
-     <input type="text" name="name" required>
-     <input type="email" name="email" required>
+<input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Type your message here"></textarea>
+    <input type="file" name="attachment" accept="image/png, image/jpeg">
      <button type="submit">Send</button>
 </form>
 """
+
 st.markdown(Contact_form,unsafe_allow_html=True)
