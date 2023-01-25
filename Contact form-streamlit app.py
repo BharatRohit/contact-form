630829e88c10b580c41cbbcf39138625a275c233
BharatRohit <94052041+BharatRohit@users.noreply.github.com>
2023-01-25 15:19:18 +0530

    Add files via upload

---
+++ Contact form-streamlit app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.header(":mailbox: Get In Touch With Me!")
+
+Contact_form = """
+<form action="https://formsubmit.co/rohitbharat810@email.com" method="POST">
+     <input type="text" name="name" required>
+     <input type="email" name="email" required>
+     <button type="submit">Send</button>
+</form>
+"""
+st.markdown(Contact_form,unsafe_allow_html=True)
