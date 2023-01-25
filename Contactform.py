4e8726080683ba36ef4db77e59b157489526f57a
BharatRohit <94052041+BharatRohit@users.noreply.github.com>
2023-01-25 15:39:35 +0530

    Update Contactform.py

--- Contactform.py
+++ Contactform.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.header(":mailbox: Get In Touch With Me!")
 
 Contact_form = """
-<form action="https://formsubmit.co/rohitbharat810@email.com" method="POST">
+<form action="https://formsubmit.co/rohitbharat810@gmail.com" method="POST">
      <input type="text" name="name" required>
      <input type="email" name="email" required>
      <button type="submit">Send</button>
